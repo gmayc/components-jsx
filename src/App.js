db734d46e8e640403416f553e30566a35ba2ddc9
Guillermo Maycotte <gmaycotte93@gmail.com>
2020-02-12 16:25:17 -0600

    init code, removed state

--- src/App.js
+++ src/App.js
@@ -3,14 +3,6 @@ import './App.css';
 import React from 'react';
 
 class App extends React.Component {
-  constructor() {
-    super();
-    this.state = {
-      counter: 0,
-      max: 100,
-    };
-  }
-
   render() {
     return (
       <div className='App'>
